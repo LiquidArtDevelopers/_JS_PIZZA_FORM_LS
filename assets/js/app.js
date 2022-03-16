26321cd2451428d76931cda1ad6eb16f0080b9b2
MDMGN <michaelmdvrhack.com>
2022-03-16 22:30:31 +0100

    implement-carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,15 @@ import dataCookies from "./dataCookies.js";
 import carousel from "./carousel.js";
 
 const d=document;
-const punto=document.querySelectorAll('.puntos li');
 d.addEventListener('DOMContentLoaded',()=>{
     //Llamar a la animación de carga.
-    loadAnimation();
+    //loadAnimation();
     //Script que gestiona las cookies.
     dataCookies();
     //Llamar a componentes.
     componentes();
     //carrousel
-    //carousel();
+    carousel();
     //Validación de formulario.
     formulario();
     //pedido
--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,26 +1,32 @@
-const grande    = document.querySelector('.frame');
-const punto     = document.querySelectorAll('.puntos li');
-export default function carrousel(position){
-    setInterval(moverCarrosel(position),3000);
-    // Asignamos un CLICK a cadaPunto
-    punto[position].addEventListener('click',(e)=>{
-        moverCarrosel(position);
+const frame= document.querySelector('.frame');
+const punto= document.querySelectorAll('.puntos li');
+let ps=0, timeMiliseg=5000;
+export default function carrousel(){
+    window.addEventListener('load',()=>{
+        setInterval(autoCarousel,timeMiliseg);
+    });
+    punto.forEach((el,position)=>{
+        // Asignamos un CLICK a cadaPunto
+        punto[position].addEventListener('click',()=>{
+            ps=position;
+            moverCarrosel(position);
+        });
     });
 }
-
-const moverCarrosel = (position)=>{
-     // Guardar la posición de ese PUNTO
+const autoCarousel=()=>{
+    (ps<3) ? ps++ : ps=0;
+    moverCarrosel(ps);
+}
+const moverCarrosel =(position)=>{
     // Calculando el espacio que debe DESPLAZARSE el FRAME
-    let operacion = position * -25;
-
+    let operation = position * -25;
     // MOVEMOS el frame
-    grande.style.transform = `translateX(${ operacion }%)`
-
+    frame.style.transform = `translateX(${ operation }%)`
     // Recorremos TODOS los punto
     punto.forEach( ( cadaPunto , i )=>{
         // Quitamos la clase ACTIVO a TODOS los punto
         punto[i].classList.remove('activo')
     })
     // Añadir la clase activo en el punto que hemos hecho CLICK
-    punto[position].classList.add('activo');      
+    punto[position].classList.add('activo');
 }
